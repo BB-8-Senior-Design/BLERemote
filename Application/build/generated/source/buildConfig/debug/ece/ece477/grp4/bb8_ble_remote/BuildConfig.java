336f7230ad564a8e3bb853cdfdc12e67fe96a04c
cychow <cchow@purdue.edu>
2016-10-25 20:50:17 -0400

    First commit of new BLE app

---
+++ Application/build/generated/source/buildConfig/debug/ece/ece477/grp4/bb8_ble_remote/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ece.ece477.grp4.bb8_ble_remote;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ece.ece477.grp4.bb8_ble_remote";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
